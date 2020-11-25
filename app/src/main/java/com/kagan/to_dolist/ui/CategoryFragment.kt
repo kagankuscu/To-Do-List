b308a763099f2ff20d602b490cf24a6b320a2cc5
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-11-25 22:06:39 +0200

    #5 Category can  be add only one time.

--- app/src/main/java/com/kagan/to_dolist/ui/CategoryFragment.kt
+++ app/src/main/java/com/kagan/to_dolist/ui/CategoryFragment.kt
@@ -18,7 +18,7 @@ class CategoryFragment : Fragment(R.layout.fragment_category) {
     private val TAG = "CategoryFragment"
     private lateinit var binding: FragmentCategoryBinding
     private lateinit var layout: View
-
+    private var category = ArrayList<String>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -31,31 +31,31 @@ class CategoryFragment : Fragment(R.layout.fragment_category) {
 
     private fun addView() {
 
-        val l = AlertDialog.Builder(context)
+        val alertDialog = AlertDialog.Builder(context)
             .setTitle(getString(R.string.add_category))
             .setSingleChoiceItems(R.array.categories,
                 -1,
                 DialogInterface.OnClickListener { dialog, which ->
-                    val d = dialog as AlertDialog
-                    d.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = true
-
-                    when (which) {
-                        0 -> {
-                            layout =
-                                layoutInflater.inflate(R.layout.category_personal, null, false)
-                        }
-                        1 -> {
-                            layout =
-                                layoutInflater.inflate(R.layout.category_work, null, false)
-                        }
-                        2 -> layout =
-                            layoutInflater.inflate(R.layout.category_meeting, null, false)
-                        3 -> layout =
-                            layoutInflater.inflate(R.layout.category_shopping, null, false)
-                        4 -> layout =
-                            layoutInflater.inflate(R.layout.category_study, null, false)
+                    dialog as AlertDialog
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = true
+                    val categoryArray = resources.getStringArray(R.array.categories)[which]
+                    val find = category.find {
+                        return@find it == categoryArray
                     }
-                    setCardViewClickListener()
+
+                    Log.d(TAG, "addView: $find")
+
+                    if (find == null) {
+                        setLayout(which)
+                        setCardViewClickListener()
+                    } else {
+                        Toast.makeText(
+                            context,
+                            getString(R.string.category_already_exists),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false
+                    }
                 })
             .setPositiveButton(
                 getString(R.string.btn_add),
@@ -63,6 +63,7 @@ class CategoryFragment : Fragment(R.layout.fragment_category) {
                     if (this::layout.isInitialized) {
                         try {
                             binding.gLCategory.addView(layout)
+                            category.add(layout.findViewById<TextView>(R.id.tvCategory).text.toString())
                         } catch (e: Exception) {
                             Sentry.captureMessage(e.message.toString())
                         }
@@ -70,7 +71,26 @@ class CategoryFragment : Fragment(R.layout.fragment_category) {
                 })
             .show()
 
-        l.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false
+    }
+
+    private fun setLayout(which: Int) {
+        when (which) {
+            0 -> {
+                layout =
+                    layoutInflater.inflate(R.layout.category_personal, null, false)
+            }
+            1 -> {
+                layout =
+                    layoutInflater.inflate(R.layout.category_work, null, false)
+            }
+            2 -> layout =
+                layoutInflater.inflate(R.layout.category_meeting, null, false)
+            3 -> layout =
+                layoutInflater.inflate(R.layout.category_shopping, null, false)
+            4 -> layout =
+                layoutInflater.inflate(R.layout.category_study, null, false)
+        }
     }
 
     private fun setCardViewClickListener() {
