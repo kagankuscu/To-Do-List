95f263191f793142d6903f12f873e810908844b2
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-11-26 17:45:11 +0200

    #7 you can add the task randomly.

--- app/src/main/java/com/kagan/to_dolist/ui/TaskFragment.kt
+++ app/src/main/java/com/kagan/to_dolist/ui/TaskFragment.kt
@@ -1,16 +1,40 @@
 package com.kagan.to_dolist.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kagan.to_dolist.R
+import com.kagan.to_dolist.databinding.FragmentTaskBinding
+import kotlin.random.Random
 
-class TaskFragment : Fragment() {
+class TaskFragment : Fragment(R.layout.fragment_task) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    val TAG = "TaskFragment"
+    private lateinit var binding: FragmentTaskBinding
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentTaskBinding.bind(view)
+
+        binding.btnAdd.setOnClickListener {
+            addTask()
+        }
+    }
+
+    private fun addTask() {
+        var layout: View? = null
+        when (Random.nextInt(0, 5)) {
+            0 -> layout = layoutInflater.inflate(R.layout.task_personal, null, false)
+            1 -> layout = layoutInflater.inflate(R.layout.task_meeting, null, false)
+            2 -> layout = layoutInflater.inflate(R.layout.task_shopping, null, false)
+            3 -> layout = layoutInflater.inflate(R.layout.task_study, null, false)
+            4 -> layout = layoutInflater.inflate(R.layout.task_work, null, false)
+        }
+
+        layout?.let {
+            binding.gLTask.addView(it)
+        }
     }
 }
