03e06902f029146977ed8d79702d34e8c8ad8043
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-12-03 14:57:54 +0200

    #19 create function overwritten

--- app/src/main/java/com/kagan/to_dolist/viewModels/viewModelFactory/TaskViewModelFactory.kt
+++ app/src/main/java/com/kagan/to_dolist/viewModels/viewModelFactory/TaskViewModelFactory.kt
@@ -3,15 +3,17 @@ package com.kagan.to_dolist.viewModels.viewModelFactory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.kagan.to_dolist.repositories.CategoryRepository
+import com.kagan.to_dolist.repositories.TaskRepository
 import com.kagan.to_dolist.viewModels.CategoryViewModel
+import com.kagan.to_dolist.viewModels.TaskViewModel
 
 @Suppress("UNCHECKED_CAST")
-class CategoryViewModelFactory(private val repository: CategoryRepository) :
+class TaskViewModelFactory(private val repository: TaskRepository) :
     ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(CategoryViewModel::class.java)) {
-            return CategoryViewModel(repository) as T
+        if (modelClass.isAssignableFrom(TaskViewModel::class.java)) {
+            return TaskViewModel(repository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
