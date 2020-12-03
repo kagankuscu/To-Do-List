e4cbeee416e6111c1c3ea3254f536b9bd65e4c12
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-12-03 19:26:17 +0200

    #20 finally i can save the new task the mock db

--- app/src/main/java/com/kagan/to_dolist/dao/TaskDao.kt
+++ app/src/main/java/com/kagan/to_dolist/dao/TaskDao.kt
@@ -4,9 +4,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.kagan.to_dolist.enums.Category
 import com.kagan.to_dolist.models.Task
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.withContext
 
 class TaskDao {
 
+    private val tasksByCategoryList = arrayListOf<Task>()
     private val tasksByCategory = MutableLiveData<ArrayList<Task>>()
 
     fun getAllTask(): LiveData<ArrayList<Task>> {
@@ -17,7 +20,10 @@ class TaskDao {
         return tasksByCategory
     }
 
-    fun save(task: Task) {
-        tasksByCategory.value?.add(task)
+    suspend fun save(task: Task) {
+        tasksByCategoryList.add(task)
+        withContext(Main) {
+            tasksByCategory.value = tasksByCategoryList
+        }
     }
 }
--- app/src/main/java/com/kagan/to_dolist/db/TaskDB.kt
+++ app/src/main/java/com/kagan/to_dolist/db/TaskDB.kt
@@ -3,5 +3,16 @@ package com.kagan.to_dolist.db
 import com.kagan.to_dolist.dao.TaskDao
 
 class TaskDB {
-    fun getTaskDao(): TaskDao = TaskDao()
+    var taskDao = TaskDao()
+        private set
+
+    companion object {
+        @Volatile
+        private var instance: TaskDB? = null
+
+        fun getInstance() =
+            instance ?: synchronized(this) {
+                instance ?: TaskDB().also { instance = it }
+            }
+    }
 }
