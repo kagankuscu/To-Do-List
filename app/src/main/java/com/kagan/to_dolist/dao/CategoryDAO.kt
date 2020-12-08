2534375c9b9e5bbcf68bc6856cea56daed2b4fe5
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-12-08 21:32:33 +0200

    #26 Category Dao same query added.

--- app/src/main/java/com/kagan/to_dolist/dao/CategoryDAO.kt
+++ app/src/main/java/com/kagan/to_dolist/dao/CategoryDAO.kt
@@ -1,4 +1,14 @@
 package com.kagan.to_dolist.dao
 
-class CategoryDAO {
+import androidx.room.Dao
+import androidx.room.Query
+import com.kagan.to_dolist.enums.Category
+
+@Dao
+interface CategoryDAO {
+
+
+    // Task Total
+    @Query(value = "SELECT COUNT(id) from task_table WHERE category LIKE :category")
+    fun getTotalTaskByCategory(category: Category)
 }
